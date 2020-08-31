c7de62ca720a2e986891237739ba7a9cc123415d
Elles <lxewk@me.com>
2020-08-31 21:00:32 +0200

    Added associations Added controllers: Auth and Test Added payload: request & response package (DTO) Added service: security package with jwt, AuthorizationService and UserDetailsImpl

---
+++ Backend/Stage-management/src/main/java/nl/kortekaas/Stagemanagement/service/AuthorizationService.java
@@ -0,0 +1,50 @@
+package nl.kortekaas.Stagemanagement.service;
+
+import nl.kortekaas.Stagemanagement.persistence.AccountRepository;
+import nl.kortekaas.Stagemanagement.persistence.UserRepository;
+import nl.kortekaas.Stagemanagement.service.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
+public class AuthorizationService {
+
+    private static final String ROLE_NOT_FOUND_ERROR = "Error: Role is not found.";
+
+    private UserRepository userRepository;
+    private AccountRepository accountRepository;
+    private PasswordEncoder encoder;
+    private AuthenticationManager authenticationManager;
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setEncoder(PasswordEncoder passwordEncoder) {
+        this.encoder = passwordEncoder;
+    }
+
+    @Autowired
+    public void setAccountRepository(AccountRepository accountRepository) { this.accountRepository = accountRepository; }
+
+    @Autowired
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Autowired
+    public void setJwtUtils(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+
+}
+
+
