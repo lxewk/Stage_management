b6c2635e0a5f2732c91ea548efa394ad81535134
Elles <lxewk@me.com>
2020-09-02 23:17:43 +0200

    add IAccountService adjust Repositories : extends JpaRepository<AccountRepository, Long> to <Account, Long> AccountService : added method addAccount(Account tempAccount)

---
+++ Backend/Stage-management/src/main/java/nl/kortekaas/Stagemanagement/service/IAccountService.java
@@ -0,0 +1,8 @@
+package nl.kortekaas.Stagemanagement.service;
+
+import nl.kortekaas.Stagemanagement.domain.Account;
+
+public interface IAccountService {
+
+    Account addAccount(Account tempAccount);
+}
